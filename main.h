6b0d21c9ff8a75cf34fb5c3b4cfbd5739f3f9763
Pasitik <paasitik@gmail.com>
2023-03-28 19:10:32 -0700

    update

--- main.h
+++ main.h
@@ -4,9 +4,11 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdarg.h>
+#include <stdlib.h>
 
 void _putchar(char *c);
 int _strlen(char *s);
 void print_arg(char format, va_list args, int *count);
 int _printf(const char *format, ...);
+int _print_integer(int n);
 #endif /* MAIN_H */
